01a3748f232ff843a835183947ae209fae7e4049
Satoshi972 <ruddy.marieluce@gmail.com>
2017-03-03 14:47:56 -0400

    Create product fini
    Verification, et ajout de produit

--- create_product.php
+++ create_product.php
@@ -2,7 +2,18 @@
 
 require_once 'inc/connect.php';
 
+#définition de quelques variabl pour gerer les images
+$maxSize = (1024 * 1000) * 2; // Taille maximum du fichier
+$uploadDir = 'uploads/'; // Répertoire d'upload
+$mimeTypeAvailable = ['image/jpg', 'image/jpeg', 'image/pjpeg', 'image/png', 'image/gif'];
+
+#variable pour gerer le formulaire
+$post = [];
+$errors = [];
+
+#requette de selection pour l'affichage des catégorie
 $category = $bdd->prepare('SELECT * FROM category');
+
 if($category->execute())
 {
 	$cat = $category->fetchAll(PDO::FETCH_ASSOC);
@@ -12,6 +23,105 @@ else
 	var_dump($category->errorInfo());
 }
 
+#verification de mes données
+if(!empty($_POST))
+{
+	$post = array_map('trim', array_map('strip_tags', $_POST)); //equivalent du foreach pour nettoyage
+
+	if(strlen($post['title']) <3)
+	{
+		$error[] = 'Le libellé est trop court';
+	}
+	if(strlen($post['description'])<10)
+	{
+		$error[] = 'La description doit faire 10 miniutes';
+	}
+	if(!is_numeric($post['ht']) || $post['ht'] < 0)
+	{
+		$error[] = 'Le prix doit être indiqué';
+	}
+	if(!is_numeric($post['tva']) || $post['tva'] < 5.5 ||$post['tva']>20)
+	{
+		$error[] = 'La tva doit être indiqué';
+	}
+	if(!is_numeric($post['category']) || $post['category'] < 0)
+	{
+		$error[] = 'La tva doit être indiqué';
+	}
+
+	if(isset($_FILES['picture']) && $_FILES['picture']['error'] === 0)
+	{
+
+		$finfo = new finfo(); //déclaration d'un objet de type finfo
+		$mimeType = $finfo->file($_FILES['picture']['tmp_name'], FILEINFO_MIME_TYPE); // récuperation du type mime du fichier, cette façon de faire est la plus sécure
+
+		$extension = pathinfo($_FILES['picture']['name'], PATHINFO_EXTENSION);//Récuperer l'extension du ficher grace au path info
+
+		if(in_array($mimeType, $mimeTypeAvailable))
+		{
+			if($_FILES['picture']['size'] <= $maxSize)
+			{
+				if(!is_dir($uploadDir))
+				{
+					mkdir($uploadDir, 0755);//création du dossier via le CHmod, permet d'avoir les droit d'ecriture
+				}
+
+				$newPictureName = uniqid('picture_').'.'.$extension;//changeent du nom du fichier avec le prefixe avatar et lui donnant un id unique. Adie les remplacement
+
+				if(!move_uploaded_file($_FILES['picture']['tmp_name'], $uploadDir.$newPictureName))
+				{
+					$errors[] = 'Erreur lors de l\'upload de la photo';
+				}
+			}
+
+			else 
+			{
+				$errors[] = 'La taille du fichier excède 2 Mo';
+			}
+		}
+
+		else 
+		{
+			$errors[] = 'Le fichier n\'est pas une image valide';
+		}
+	}
+	else 
+	{
+		$errors[] = 'Erreur d\'envoi du fichier';
+	}
+
+	if(!is_numeric($post['category']))
+	{
+		$errors[] = 'La catégorie doit être indiqué';
+	}
+	
+	#s'il n'y a pas d'erreur
+	if(count($errors) === 0)
+	{
+		$insert = $bdd->prepare('INSERT INTO products (pdt_title, pdt_description, pdt_ref, pdt_ht, pdt_tva, pdt_picture, pdt_cat_id) VALUES(:pdt_title, :pdt_description, :pdt_ref, :pdt_ht, :pdt_tva, :pdt_picture, :pdt_cat_id)');
+		$insert->bindValue(':pdt_title',$post['title']);
+		$insert->bindValue(':pdt_description',$post['description']);
+		$insert->bindValue(':pdt_ref',$post['ref']);
+		$insert->bindValue(':pdt_ht',$post['ht'],PDO::PARAM_INT);
+		$insert->bindValue(':pdt_tva',$post['tva'],PDO::PARAM_INT);
+		$insert->bindValue(':pdt_picture',$uploadDir.$newPictureName);
+		$insert->bindValue(':pdt_cat_id',$post['category'],PDO::PARAM_INT);
+
+		if($insert->execute())
+		{
+			header('Location: list_product.php');
+		}
+		else
+		{
+			var_dump($insert->errorInfo());
+		}
+	#sinon affiche les erreurs
+	}
+	else
+	{
+		$textError = implode('<p class="alert alert-danger">', $errors);
+	}
+}
 
 ?><!DOCTYPE html>
 <html lang="fr">
@@ -25,8 +135,13 @@ else
 		<?php include 'inc/menu.php';?>
 	
 		<form method="post" class="form-horizontal jumbotron" enctype="multipart/form-data">
+
+			<legend class="text-center">Nouveau produit</legend>
+
+			<?php if(isset($textError)){echo $textError;} ?>
+
 			<div class="form-group">
-				<label for="title">Libéllé du produit</label>
+				<label for="title">Libellé du produit</label>
 				<input type="text" name="title" id="title" class="form-control">
 			</div>
 
@@ -36,7 +151,7 @@ else
 			</div>
 
 			<div class="form-group">
-				<label for="ref">Référence du prodduit</label>
+				<label for="ref">Référence du produit</label>
 				<input type="text" name="ref" id="ref" class="form-control">
 			</div>
 
@@ -68,7 +183,7 @@ else
 				<?php 
 					foreach ($cat as $key=> $value):
 					?>
-						<option value="<?php echo $key['cat_id'];?>"> <?php echo $value['cat_name']?></option>
+						<option value="<?php echo $value['cat_id'];?>"> <?php echo $value['cat_name']?></option>
 					<?php
 					endforeach;
 				?>
@@ -78,7 +193,7 @@ else
 			</div>
 
 			<div class="text-center">
-				<input type="sumbit" value="Stocker" class="btn btn-primary">
+				<input type="submit" value="Stocker" class="btn btn-primary">
 			</div>
 		</form>
 
